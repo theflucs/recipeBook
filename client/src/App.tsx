60c8eaef4a4c1bf99603c161711db06eb0552146
sabrinacux <sa.cux.dev@gmail.com>
2024-06-23 19:26:11 +0200

    chore: add tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p className="text-red-500 font-bold underline">Hello Tailwind!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
