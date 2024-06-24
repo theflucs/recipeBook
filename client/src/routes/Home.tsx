e0a91a66c6409be0f13aa86f17bd96f7959cfc54
sabrinacux <sa.cux.dev@gmail.com>
2024-06-24 22:58:43 +0200

    style homepage

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { Recipe } from "../types/api";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getRecipes } from "../api.ts/calls";
+import { BASE_API_URL } from "../api.ts/BASE_API_URL";
 
 function Home() {
   const {
@@ -22,28 +23,48 @@ function Home() {
       return allPages.length + 1;
     },
   });
+
   const handleClick = () => {
     fetchNextPage();
   };
 
   return (
     <section>
-      <h1>Home</h1>
       {isError && <p>Error</p>}
       {isFetching && <p>Loading...</p>}
-      <ul>
+      <div className="bg-gray-100 flex items-center justify-center text-center">
+        <div className="px-2 py-4">
+          <h2 className="text-lg md:text-2xl">Welcome to your Recipe Book</h2>
+          <h3 className="text-md md:text-xl font-semibold">Explore our recipes online!</h3>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {isFetched &&
           data?.pages?.map((page: Recipe[]) =>
             page.map((recipe: Recipe) => {
+              const { id, name, image } = recipe;
               return (
-                <li key={recipe.id} className="py-2">
-                  {recipe.name}
-                </li>
+                <article
+                  key={id}
+                  className="max-w-xs w-full overflow-hidden shadow-lg bg-white flex flex-col justify-between mx-auto mb-4"
+                >
+                  <div className="w-full h-48 overflow-hidden rounded-md">
+                    <img
+                      loading="lazy"
+                      className="w-full h-full object-cover"
+                      src={`${BASE_API_URL}${image}`}
+                      alt={name}
+                    />
+                  </div>
+                  <div className="px-6 py-4 flex flex-col justify-center">
+                    <div className="font-bold text-xl mb-2">{name}</div>
+                  </div>
+                </article>
               );
             })
           )}
-      </ul>
-      <div>
+      </div>
+      <div className="my-6 text-center">
         <button
           onClick={() => handleClick()}
           disabled={!hasNextPage || isFetchingNextPage}
