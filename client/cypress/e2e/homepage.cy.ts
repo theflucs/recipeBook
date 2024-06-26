c56cea16a9c3fb1a45fc88a3d45cabd803b061c8
sabrinacux <sa.cux.dev@gmail.com>
2024-06-26 15:16:26 +0200

    add tests for initial ui elements render in homepage

--- client/cypress/e2e/homepage.cy.ts
+++ client/cypress/e2e/homepage.cy.ts
@@ -1,6 +1,26 @@
-describe("Check app title", () => {
-  it("should have the correct app title", () => {
-    cy.visit('/')
+describe('initial ui elements render', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it("should display the correct app title", () => {
     cy.title().should("include", "Recipe Book");
   });
+  it("should have the correct homepage title", () => {
+    cy.get('#homepage-title').should('be.visible');
+  });
+  it("should have the correct homepage subtitle", () => {
+    cy.get('#homepage-subtitle').should('be.visible');
+  });
+  it('should display the search input field', () => {
+    cy.get('#search-bar').should('be.visible');
+  });
+
+  it('should display the "Reset Filters" button', () => {
+    cy.get('#reset-filters-button')
+      .contains('Reset Filters')
+      .should('be.visible');
+  });
+});
+
 });
