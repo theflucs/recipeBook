ef06e1650d4ee7a07a63e0c93d68e43f66afc5ca
sabrinacux <sa.cux.dev@gmail.com>
2024-06-26 10:43:46 +0200

    add simple nav from recipe detail to home

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <div className="bg-gray-100">
-      <header className="bg-white shadow-sm">
-        <div className="max-w-7xl mx-auto px-4 py-3">
+      <header className="bg-amber-400 shadow-sm text-white">
+        <div className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3">
           <h1 className="text-lg font-semibold">Online Recipe Book</h1>
+          {!isHomePage && (
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/" className="text-gray-600 hover:text-gray-900">
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
       </header>
 
