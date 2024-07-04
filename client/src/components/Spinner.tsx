a8224a1aaac9de4a6ecbd71708b2e5eba5fd209e
sabrinacux <sa.cux.dev@gmail.com>
2024-07-04 13:11:33 +0200

    created global spinner

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,16 @@
+function Spinner() {
+  return (
+    <div className="flex justify-center h-screen">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-amber-600 border-e-transparent align-[-0.125em] text-amber-600 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
